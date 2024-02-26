29b2d0ee936586b1d3b9d3e9b2f0fc8929e49ccb
sonasol2 <57984322+sonasol2@users.noreply.github.com>
2024-02-26 18:44:02 +0200

    update

--- Console Game/NPC.cs
+++ Console Game/NPC.cs
@@ -1,11 +1,30 @@
 ﻿using System;
 namespace Console_Game
 {
-	public class NPC
+	class NPC : Units
 	{
-		public NPC()
+		public NPC(int x, int y)
 		{
-		}
-	}
+            X = x;
+            Y = y;
+        }
+
+        public override int Health => throw new NotImplementedException();
+
+        public override int X { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public override int Y { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public override void Hit()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Move(int xTarget, int yTarget)
+        {
+            throw new NotImplementedException();
+        }
+
+        delegate void NoGamePlayer();
+    }
 }
 
